85b08bc226345c33f27c1b8b363ecc8ff52b70ff
Daniel Doughty <daniel.doughty@hotmail.com>
2016-05-03 18:08:29 +0000

    Added goodbye action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,13 @@ class ApplicationController < ActionController::Base
   def goodbye
     render text: "goodbye, world!"
   end
+  
+  def cool(text)
+    puts "We can square any number. Enter a number"
+    text = gets.chomp
+    puts text**2
+  end
+    
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
