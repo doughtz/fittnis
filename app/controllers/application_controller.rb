51ad445bad90d6b3bcf5459b71bebe2b72f193b2
Daniel Doughty <daniel.doughty@hotmail.com>
2016-05-09 18:51:17 +0000

    First commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,7 @@ class ApplicationController < ActionController::Base
   def hello
     render text: "hello,world!"
   end
-  def goodbye
-    render text: "goodbye, world!"
-  end
-  
-  def cool
-    render text: "This will hopefully become cool!"
-  end
+
     
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
