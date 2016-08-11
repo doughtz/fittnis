66db22522286800d60d30824b371a1607bbe1425
Daniel Doughty <daniel.doughty@hotmail.com>
2016-08-11 17:36:16 +0000

    Added workoutpoints model and adds when video complete

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,25 @@ module UsersHelper
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
   
+  # corrects nil workouts if present
+  def correct_workout_nil
+    if @user.workouts == nil
+      @user.workouts = 0
+    end
+  end
+  
+  
+  def update_workouts_and_new_workoutpoint
+    if logged_in?
+        correct_workout_nil
+        @user.workouts += 1
+        @user.save
+        @workoutpoint = @user.workoutpoints.build(workout_point: 1)
+        @workoutpoint.save
+        render(json: { message: "Workouts increased" }, status: :ok) and return
+      else 
+        render(json: { message: "Workouts increased" }, status: :ok) and return
+    end
+  end
+  
 end
---
+++ test/models/workoutpoint_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WorkoutpointTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
