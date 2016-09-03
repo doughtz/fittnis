cdc97541e4a11853a0e53b42b4819fa75390be7d
Daniel Doughty <daniel.doughty@hotmail.com>
2016-09-03 03:06:29 +0000

    add user profile to header menu

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160828192152) do
+ActiveRecord::Schema.define(version: 20160903022745) do
 
   create_table "calspersecs", force: :cascade do |t|
     t.float    "calories_persec"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20160828192152) do
     t.string   "provider"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
